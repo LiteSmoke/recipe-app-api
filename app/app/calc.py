51e17fa51a97eb63c7daf9eb25af32a7588751f6
Pavel Legkodymov <legkodymov1986@mail.ru>
2023-03-13 13:26:19 +0100

    Added wait_for_db before calling DB

---
+++ app/app/calc.py
@@ -0,0 +1,14 @@
+# noqa
+"""
+Calculator functions
+"""
+
+
+def add(x, y):
+    """ Add x and y and return result """
+    return x + y
+
+
+def subtract(x, y):
+    """Subtracts x from y and returns result"""
+    return y - x
